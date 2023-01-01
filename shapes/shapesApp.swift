1ef0774dbd2e3f6c9da12db82955d12f5a18c1ef
Aniket Singh <31aniket.singh@gmail.com>
2023-01-01 19:59:46 +0530

    Initial Commit

---
+++ shapes/shapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  shapesApp.swift
+//  shapes
+//
+//  Created by Aniket Singh on 01/01/23.
+//
+
+import SwiftUI
+
+@main
+struct shapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
